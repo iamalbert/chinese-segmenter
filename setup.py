cb45986c4a5998e4e883a3c98474d3b738bf3170
Wen Li Zhuang <wlzhuang@nlg.csie.ntu.edu.tw>
2015-08-03 11:07:22 +0800

    add trove classifier: python3 & nlp

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name = 'chineseseg',
-  packages = ['chineseseg'], # this must be the same as the name above
+  packages = ['chineseseg'], 
   version = '0.1',
   description = 'A Python3 wrapper for a collection of Chinese word segmenter',
   author = 'WenLi Zhuang',
@@ -9,5 +9,9 @@ setup(
   url = 'https://github.com/iamalbert/chinese-segmenter', 
   download_url = 'https://github.com/iamalbert/chinese-second/tarball/0.1', 
   keywords = ['nlp','chinese', 'word', 'segment', 'text'],
-  classifiers = [],
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "Natural Language :: Chinese (Simplified)",
+    "Natural Language :: Chinese (Traditional)"
+  ],
 )
